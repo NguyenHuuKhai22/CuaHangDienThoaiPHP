ab62a26dec3a0f8c867df0c23996e47a1c84bff7
Nguyen Huu Khai <nguyenhuukhai22052003@gmail.com>
2025-04-20 15:31:54 +0700

    web php laravel

--- phone_store/config/openai.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'api_key' => env('OPENAI_API_KEY'),
-    'organization' => env('OPENAI_ORGANIZATION'),
-    'model' => 'gpt-3.5-turbo',
-    'temperature' => 0.7,
-    'max_tokens' => 1000,
-]; 
